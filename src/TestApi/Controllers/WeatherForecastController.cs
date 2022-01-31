f49881709b3a83364647ec510b6eb0a8f5ee03ee
nayan sarkar <nayan.sarkar@serko.com>
2022-02-01 00:15:25 +1300

    Test commit

--- src/TestApi/Controllers/WeatherForecastController.cs
+++ src/TestApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "FreezingTest", "BracingTest", "ChillyTest", "CoolTest", "MildTest", "WarmTest", "BalmyTest", "HotTest", "SwelteringTest", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
